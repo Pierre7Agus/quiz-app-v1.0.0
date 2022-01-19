6549fc97f5e047532196347e4d00c6b6e0a2ab59
Pierre7Agus <redtor_90@outlook.es>
2022-01-19 18:13:44 -0500

    Modify package.json and questions.js The styles of the questions.js file were modified using standard Js, and in package.json the "fix" script was added, which will allow adjusting the writing styles of the code

--- data/questions.js
+++ data/questions.js
@@ -1,35 +1,35 @@
 export const questions = [
   {
-    question:"¿Qué es HTML?",
-    choices:["Es un lenguaje de marcado que permite dar estructura a páginas y aplicaciones web",
-  "Es un lenguaje de programación","Es un editor de Código","Es un IDE"],
-    answer:"Es un lenguaje de marcado que permite dar estructura a páginas y aplicaciones web",
+    question: '¿Qué es HTML?',
+    choices: ['Es un lenguaje de marcado que permite dar estructura a páginas y aplicaciones web',
+      'Es un lenguaje de programación', 'Es un editor de Código', 'Es un IDE'],
+    answer: 'Es un lenguaje de marcado que permite dar estructura a páginas y aplicaciones web'
   },
   {
-    question:"¿Qué es un IDE?",
-    choices:["Es un entorno de desarrollo integrado","Es un entrono de herramientas",
-  "Es un compilador","Es una teoría del origen del universo"],
-    answer:"Es un entorno de desarrollo integrado",
+    question: '¿Qué es un IDE?',
+    choices: ['Es un entorno de desarrollo integrado', 'Es un entrono de herramientas',
+      'Es un compilador', 'Es una teoría del origen del universo'],
+    answer: 'Es un entorno de desarrollo integrado'
   },
   {
-    question:"Cuál de los siguientes es un lenguaje de Programación Compilado",
-    choices:["React","Firefox","Rust","CSS"],
-    answer:"Rust",
+    question: 'Cuál de los siguientes es un lenguaje de Programación Compilado',
+    choices: ['React', 'Firefox', 'Rust', 'CSS'],
+    answer: 'Rust'
   },
   {
-    question:"Cuál de los siguientes es un lenguaje de Programación Interpretado",
-    choices:["JavaScript","NodeJs","C++","Cobol"],
-    answer:"JavaScript",
+    question: 'Cuál de los siguientes es un lenguaje de Programación Interpretado',
+    choices: ['JavaScript', 'NodeJs', 'C++', 'Cobol'],
+    answer: 'JavaScript'
   },
   {
-    question:"¿Qué es Git?",
-    choices:["Es un sistema de control de versiones","Un entorno de Desarrollo","Una aplicación Web",
-    "Una serie de televisión"],
-    answer:"Es un sistema de control de versiones",
+    question: '¿Qué es Git?',
+    choices: ['Es un sistema de control de versiones', 'Un entorno de Desarrollo', 'Una aplicación Web',
+      'Una serie de televisión'],
+    answer: 'Es un sistema de control de versiones'
   },
   {
-    question:"¿Que permite hacer Lit?",
-    choices:["Componentes Web","Diagramas de flujo","Graficos 3D","Juegos 2D"],
-    answer:"Componentes Web",
-  },
-];
+    question: '¿Que permite hacer Lit?',
+    choices: ['Componentes Web', 'Diagramas de flujo', 'Graficos 3D', 'Juegos 2D'],
+    answer: 'Componentes Web'
+  }
+]
