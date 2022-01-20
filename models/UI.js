80a738cfa6865a9211601f01c01f8b6045ae17a8
Pierre7Agus <redtor_90@outlook.es>
2022-01-20 17:19:17 -0500

    Add showChoices method This method allows to show the answer options, it also allows to capture the text of the option that the user chooses

--- models/UI.js
+++ models/UI.js
@@ -6,4 +6,21 @@ class UI{
     question.innerText=textQuestion
   }
 
+  showChoices(choices,callback){
+  //choices es un array
+    let buttons=document.querySelectorAll('.options')
+    let parentElement=document.querySelector('.quiz_section_options')
+
+    parentElement.addEventListener('click',(event)=>{
+      if(event.target.className === "options"){
+        callback(event.target.innerText)
+      }
+      event.stopPropagation()
+    })
+
+    choices.forEach((choice,index)=>{
+      buttons[index].innerText=choice
+    })
+  }
+
 }
