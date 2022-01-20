af7608681f48008d649de2f4d85f1f148ccef4b2
Pierre7Agus <redtor_90@outlook.es>
2022-01-20 15:20:25 -0500

    Add guess method This method allows to know if the user chose the correct answer, if so, the score variable is increased by one and at the end whenever this method is executed, the index variable is increased by one

--- models/QuizLogic.js
+++ models/QuizLogic.js
@@ -1,6 +1,6 @@
 class QuizLogic{
-  let index=0;
-  let score=0;
+  index=0;
+  score=0;
   //recibe como parametro un array el cual contiene instancias de Questions.js
   constructor(quizQuestions){
     this.quizQuestions=quizQuestions;
@@ -10,6 +10,11 @@ class QuizLogic{
     return this.quizQuestions[index];
   }
 
-  
+  guess(answer){
+    if(this.getCurrentQuestion().correctAnswer(answer)){
+      this.score++;
+    }
+    this.index++;
+  }
 
 }
