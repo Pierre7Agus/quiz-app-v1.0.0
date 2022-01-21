db15b9be8c0a926235cbfdba63c04244be417578
Pierre7Agus <redtor_90@outlook.es>
2022-01-21 00:54:08 -0500

    Modify showChoices method of UI class A change was made, instead of creating the buttons with javascript now only the text node of the button elements is being modified, the event delegation was applied assigning only one addEventListener to the parent element and from there to be able to select the child elements, from this way you avoid having to build addEventListener to each button element

--- models/UI.js
+++ models/UI.js
@@ -7,6 +7,21 @@ export class UI {
   showChoices (choices, callback) {
   // choices es un array
 
+  const buttons = document.querySelectorAll('.options')
+
+  choices.forEach((choice, index) => {
+    buttons[index].innerText = choice
+  })
+
+  const parentElement=document.querySelector('.choices')
+  parentElement.addEventListener('click',(e)=>{
+    if(e.target.className === 'options'){
+        callback(event.target.innerText)
+    }
+  })
+
+    /*
+    //este codigo permite generar botones, de esta manera se puede dar la posibilidad al usuario de que elija cuantas opciones de respuesta deba tener el quiz
     const options = document.querySelector('.choices');
     options.innerHTML='';
     choices.forEach((choice)=>{
@@ -18,12 +33,7 @@ export class UI {
         button.classList.add('options');
         options.append(button);
     });
-
-    const buttons = document.querySelectorAll('.options')
-
-    choices.forEach((choice, index) => {
-      buttons[index].innerText = choice
-    })
+*/
   }
 
   showProgress (index, total) {
